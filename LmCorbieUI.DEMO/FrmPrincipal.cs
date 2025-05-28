e06eb98fc7453e29f8451c085dad87df7b1cae6c
lm-c <michalakleo@gmail.com>
2025-05-28 13:58:44 -0300

    Melhoria toast, agora o mesmo aparece emilhado na lateral

--- LmCorbieUI.DEMO/FrmPrincipal.cs
+++ LmCorbieUI.DEMO/FrmPrincipal.cs
@@ -6,50 +6,66 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LmCorbieUI.DEMO
-{
-    public partial class FrmPrincipal : LmMainForm
-    {
-        public FrmPrincipal()
-        {
-            InitializeComponent();
-        }
+namespace LmCorbieUI.DEMO {
+  public partial class FrmPrincipal : LmMainForm {
+    public FrmPrincipal() {
+      InitializeComponent();
+    }
 
-        private void lmMenuItem3_Click(object sender, EventArgs e)
-        {
+    private void lmMenuItem3_Click(object sender, EventArgs e) {
 
-        }
+    }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        }
+    private void Form1_Load(object sender, EventArgs e) {
+    }
 
-        private void menuSistema_Click(object sender, EventArgs e)
-        {
-        }
+    private void menuSistema_Click(object sender, EventArgs e) {
+    }
 
-        private void menuDadosEmpresa_Click(object sender, EventArgs e)
-        {
-            FrmSingle frm = new FrmSingle();
-            frm.ShowDialog();
-        }
+    private void menuDadosEmpresa_Click(object sender, EventArgs e) {
+      FrmSingle frm = new FrmSingle();
+      frm.ShowDialog();
+    }
 
-        private void menuUsuarioCad_Click(object sender, EventArgs e)
-        {
-            this.OpenFormChild(new FrmChild());
+    private void menuUsuarioCad_Click(object sender, EventArgs e) {
+      this.OpenFormChild(new FrmChild());
 
-        }
+    }
 
-        private void FrmPrincipal_FormClosing(object sender, FormClosingEventArgs e)
-        {
+    private void FrmPrincipal_FormClosing(object sender, FormClosingEventArgs e) {
 
-        }
+    }
 
+    int cont = 1;
     private void menuProcessoCad_Click(object sender, EventArgs e) {
-      Toast.Error("Teste de Sistema.\r\nasfdasda sdas ads\r\n´poitoy´tiyp´tyitpor\r\n409387534890347594857  43574357349087 53490\r\nfdff sdf sdsdf sdf sdf\r\nxxxxxxxxxxxxxxxxxxx\r\nccccccccccccc");
+      switch (cont) {
+        case 1:
+        Toast.Error("Teste de Sistema.\r\nasfdasda sdas ads\r\n´poitoy´tiyp´tyitpor\r\n409387534890347594857  43574357349087 53490\r\nfdff sdf sdsdf sdf sdf\r\nxxxxxxxxxxxxxxxxxxx\r\nccccccccccccc");
+        cont = 2;
+        break;
+        case 2:
+        Toast.Warning("Apenas um Olá!\r\nApenas um Olá!");
+        cont = 3;
+        break;
+        case 3:
+        Toast.Success("Apenas um Olá 2! Apenas um Olá! Apenas um Olá!");
+        cont = 4;
+        break;
+        case 4:
+        Toast.Info("Apenas um Olá 3!");
+        cont = 5;
+        break;
+        case 5:
+        Toast.Black("Apenas um Olá 4!");
+        cont = 1;
+        break;
+        default:
+        break;
+      }
     }
   }
 }
--- LmCorbieUI/02_LmMsgBox/Toast.cs
+++ LmCorbieUI/02_LmMsgBox/Toast.cs
@@ -1,16 +1,19 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace LmCorbieUI {
   public class Toast {
+    private static List<FrmToastForm> activeToasts = new List<FrmToastForm>();
 
     public static void Show(string message) {
       var backColor = Color.FromArgb(250, 248, 240);
       var foreColor = Color.FromArgb(20, 22, 30);
       var icon = new Bitmap(15, 15);
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
-      GetPosition(toast);
-      toast.Show();
+      ShowToast(toast);
     }
 
     public static void Success(string message) {
@@ -18,8 +21,7 @@ namespace LmCorbieUI {
       var foreColor = Color.FromArgb(250, 248, 240);
       var icon = Properties.Resources.toast_sucess;
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
-      GetPosition(toast);
-      toast.Show();
+      ShowToast(toast);
     }
 
     public static void Info(string message) {
@@ -27,8 +29,7 @@ namespace LmCorbieUI {
       var foreColor = Color.FromArgb(250, 248, 240);
       var icon = Properties.Resources.toast_info;
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
-      GetPosition(toast);
-      toast.Show();
+      ShowToast(toast);
     }
 
     public static void Warning(string message) {
@@ -36,8 +37,7 @@ namespace LmCorbieUI {
       var foreColor = Color.FromArgb(250, 248, 240);
       var icon = Properties.Resources.toast_warning;
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
-      GetPosition(toast);
-      toast.Show();
+      ShowToast(toast);
     }
 
     public static void Error(string message) {
@@ -45,8 +45,7 @@ namespace LmCorbieUI {
       var foreColor = Color.FromArgb(250, 248, 240);
       var icon = Properties.Resources.toast_error;
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
-      GetPosition(toast);
-      toast.Show();
+      ShowToast(toast);
     }
 
     public static void Black(string message) {
@@ -54,7 +53,22 @@ namespace LmCorbieUI {
       var foreColor = Color.FromArgb(250, 248, 240);
       var icon = new Bitmap(15, 15);
       FrmToastForm toast = new FrmToastForm(message, backColor, foreColor, icon);
+      ShowToast(toast);
+    }
+
+    private static void ShowToast(FrmToastForm toast) {
+      // Remove toasts que já foram fechados da lista
+      activeToasts.RemoveAll(t => t.IsDisposed || !t.Visible);
+
       GetPosition(toast);
+      activeToasts.Add(toast);
+
+      // Adiciona evento para remover o toast da lista quando for fechado
+      toast.FormClosed += (sender, e) => {
+        activeToasts.Remove(toast);
+        RepositionToasts();
+      };
+
       toast.Show();
     }
 
@@ -62,12 +76,34 @@ namespace LmCorbieUI {
       Point mousePosition = Cursor.Position;
       Screen currentScreen = Screen.FromPoint(mousePosition);
 
+      int yOffset = 10; // Posição inicial do topo
+
+      // Calcula o offset vertical baseado nos toasts ativos
+      foreach (var activeToast in activeToasts.Where(t => !t.IsDisposed && t.Visible)) {
+        yOffset += activeToast.Height + 5; // 5px de espaçamento entre toasts
+      }
+
       toast.Location = new Point(
         currentScreen.WorkingArea.Right - toast.Width - 10,
-        currentScreen.WorkingArea.Top + 10
+        currentScreen.WorkingArea.Top + yOffset
     );
     }
 
+    private static void RepositionToasts() {
+      // Reposiciona todos os toasts ativos quando um é removido
+      Point mousePosition = Cursor.Position;
+      Screen currentScreen = Screen.FromPoint(mousePosition);
+
+      int yOffset = 10;
+
+      foreach (var activeToast in activeToasts.Where(t => !t.IsDisposed && t.Visible)) {
+        activeToast.Location = new Point(
+          currentScreen.WorkingArea.Right - activeToast.Width - 10,
+          currentScreen.WorkingArea.Top + yOffset
+        );
+        yOffset += activeToast.Height + 5;
+      }
+    }
   }
 }
 
